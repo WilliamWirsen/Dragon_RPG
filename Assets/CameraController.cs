fe328e10e4245fb4470aaddf36b53ba0610afc0e
willi <william.wirsen@live.se>
2018-06-21 15:45:05 +0200

    Implemented two cameras (freeflow and fixed) to follow the character and the ability to switch between them.

---
+++ Assets/CameraController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class CameraController : MonoBehaviour {
+    GameObject mainCamera;
+    [SerializeField] GameObject secondaryCamera;
+
+    bool isMainCameraActive = true;
+	// Use this for initialization
+	void Start () {
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        print(mainCamera);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CameraInput();
+		
+	}
+
+    private void CameraInput()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Fire1"))
+        {
+            isMainCameraActive = !isMainCameraActive;
+            SwitchCamera();            
+        }
+    }
+
+    private void SwitchCamera()
+    {
+        if(isMainCameraActive)
+        {
+            secondaryCamera.SetActive(false);
+            mainCamera.SetActive(true);
+        } else
+        {
+            mainCamera.SetActive(false);
+            secondaryCamera.SetActive(true);
+
+        }
+        
+    }
+    
+}
