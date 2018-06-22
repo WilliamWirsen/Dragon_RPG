467e75e303c21f25fcc673025833ac5da6e8f469
willi <william.wirsen@live.se>
2018-06-22 23:49:42 +0200

    Created a nav mesh so enemies run towards the player

--- Assets/Enemies/Enemy.cs
+++ Assets/Enemies/Enemy.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
 
 public class Enemy : MonoBehaviour {
 
     [SerializeField] float maxHealthPoints = 100f;
+    [SerializeField] float attackRadius = 2f;
     float currentHealthPoints = 100f;
+    AICharacterControl aiCharacterControl = null;
+    GameObject player = null;
 
     public float healthAsPercentage
     {
@@ -13,6 +17,23 @@ public class Enemy : MonoBehaviour {
         {
             float healthAsPercentage = currentHealthPoints / maxHealthPoints;
             return healthAsPercentage; 
+        }
+    }
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        aiCharacterControl = GetComponent<AICharacterControl>();
+    }
+
+    private void Update()
+    {
+        float distanceToPlayer = Vector3.Distance(player.transform.position, aiCharacterControl.transform.position); 
+        if(distanceToPlayer <= attackRadius)
+        {
+            aiCharacterControl.SetTarget(player.transform); 
+        } else
+        {
+            aiCharacterControl.SetTarget(aiCharacterControl.transform);
         }
     }
 }
