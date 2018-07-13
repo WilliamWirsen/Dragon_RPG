ce18aa4d5fd0708fb31ec82e2dff5c54c0760958
William <william.wirsen@live.se>
2018-07-13 21:39:43 +0200

    Fixed movement issues with characters

--- Assets/Weapons/Projectile.cs
+++ Assets/Weapons/Projectile.cs
@@ -5,19 +5,20 @@ using UnityEngine;
 public class Projectile : MonoBehaviour {
 
     public float projectileSpeed;
-   float damageCaused;
+    float damageCaused;
 
     public void SetDamage(float damage)
     {
         damageCaused = damage;
     }
 
-    void OnTriggerEnter(Collider collider)
+    void OnCollisionEnter (Collision collision)
     {
-        Component damageableComponent = collider.gameObject.GetComponent(typeof(IDamageable));
-        if(damageableComponent)
+        Component damageableComponent = collision.gameObject.GetComponent(typeof(IDamageable));
+        if (damageableComponent)
         {
             (damageableComponent as IDamageable).TakeDamage(damageCaused);
         }
+        Destroy(gameObject, 0.1f);
     }
 }
