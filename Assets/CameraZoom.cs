d44621d9a64e07bb52fe08dd8025084793322e0c
William <william.wirsen@live.se>
2022-06-20 12:18:48 +0200

    Add Zoom with mouse wheel

--- Assets/CameraZoom.cs
+++ Assets/CameraZoom.cs
@@ -1,18 +1,28 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CameraZoom : MonoBehaviour
 {
+    private float _fov;
+
+    [SerializeField]
+    [Tooltip("Sensitivity of how aggressive the zoom will be")]
+    private float _sensitivity;
+
+    private const int defaultFov = 60;
+    private const int _minFov = 40;
+    private const int _maxFov = 60;    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _fov = defaultFov; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _fov -= Input.GetAxis("Mouse ScrollWheel") * _sensitivity;
+        _fov = Mathf.Clamp(_fov, _minFov, _maxFov);
+        Camera.main.fieldOfView = _fov;
     }
 }
